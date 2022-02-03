1b4b093b2619aef4f04bff38db84e9ad721ebdf3
naytonsan <sanchesnb@gmail.com>
2022-02-03 15:31:46 -0300

    update

--- atualizar.php
+++ atualizar.php
@@ -7,7 +7,7 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-$sql_atualizar = "UPDATE usuario SET nome = ($nome), email= ($email), senha= ($senha) WHERE ID_USUARIO=($id)";
+$sql_atualizar = "UPDATE usuario SET nome = '$nome', email= '$email', senha= '$senha' WHERE ID_USUARIO='$id'";
 
 mysqli_query($con, $sql_atualizar) or die(mysqli_connect_error($con));
 
--- deletar.php
+++ deletar.php
@@ -3,7 +3,7 @@ include_once dirname(__FILE__) . "/conexao.php";
 
 $id = $_POST['id'];
 
-$sql_deletar = "DELETE FROM usuario WHERE ID_USUARIO=($id)";
+$sql_deletar = "DELETE FROM usuario WHERE ID_USUARIO='$id'";
 
 mysqli_query($con, $sql_deletar) or die(mysqli_connect_error($con));
 
