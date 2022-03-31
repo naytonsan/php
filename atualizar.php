8a53eef9833beff96964a572fb080eb523e51d49
Nayton Sanches Barbosa <30939450+naytonsan@users.noreply.github.com>
2022-03-31 11:36:10 -0300

    Update atualizar.php
    update

--- atualizar.php
+++ atualizar.php
@@ -10,8 +10,3 @@ $senha = $_POST['senha'];
 $sql_atualizar = "UPDATE usuario SET nome = '$nome', email= '$email', senha= '$senha' WHERE ID_USUARIO='$id'";
 
 mysqli_query($con, $sql_atualizar) or die(mysqli_connect_error($con));
-
-// Porque o Atualizar e o Deletar funciona sem os comandos abaixo e o Salvar não ?
-
-// $response =  array("success" => true, 'NOME' => $nome);
-// echo json_encode($response);
